561da03238b79896236c0b2c1cb5b08484744d37
Simon Taranto <simon.taranto@gmail.com>
2014-02-09 23:26:32 -0700

    Add additional image rotation tests

--- solutions/taranto_simon/problem_6/image_test.rb
+++ solutions/taranto_simon/problem_6/image_test.rb
@@ -13,13 +13,34 @@ class ImageTest < Minitest::Test
     assert_equal(["1", "2", "3", "4"], image.pixels.flatten.map(&:color))
   end
 
-  def test_it_can_be_rotated_90_CW
+  def test_it_can_be_rotated_90_CW_for_2x2
     image = Image.new([
                         ["1", "2"],
                         ["3", "4"]
                       ])
     rotated = image.rotate_cw
     assert_equal(["3", "1", "4", "2"], rotated.pixels.flatten.map(&:color))
+  end
+
+  def test_it_can_be_rotated_90_CW_for_3x3
+    image = Image.new([
+                        ["1", "2", "3"],
+                        ["4", "5", "6"],
+                        ["7", "8", "9"]
+                      ])
+    rotated = image.rotate_cw
+    assert_equal(["7", "4", "1", "8", "5", "2", "9", "6", "3"], rotated.pixels.flatten.map(&:color))
+  end
+
+  def test_it_can_be_rotated_90_CW_for_4x4
+    image = Image.new([
+                        ["1", "2", "3", "4"],
+                        ["5", "6", "7", "8"],
+                        ["9", "10", "11", "12"],
+                        ["13", "14", "15", "16"]
+                      ])
+    rotated = image.rotate_cw
+    assert_equal(["13", "9", "5", "1", "14", "10", "6", "2", "15", "11", "7", "3", "16", "12", "8", "4"], rotated.pixels.flatten.map(&:color))
   end
 
 end
