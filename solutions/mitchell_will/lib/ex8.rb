be78459fba57ea33f531d10333cebc2eb796bb8c
Will Mitchell <wvmitchell@gmail.com>
2014-02-09 22:25:55 -0700

    alternative ex8 implementation

--- solutions/mitchell_will/lib/ex8.rb
+++ solutions/mitchell_will/lib/ex8.rb
@@ -4,8 +4,22 @@ class String
     self.index(other)
   end
 
+  #def is_rotation?(other)
+  #  (self + self).is_substring?(other)
+  #end
+
   def is_rotation?(other)
-    (self + self).is_substring?(other)
+    other.length.times do
+      other = rotate(other)
+      return true if other == self
+    end
+    false
+  end
+
+  private
+
+  def rotate(word)
+    word[-1] + word[0..-2]
   end
 
 end
